bb6679cb5253d27c1434b6d62e52901226ddcfe0
Rodrigo E. Principe <fitoprincipe82@gmail.com>
2017-11-28 10:17:39 -0300

    add code

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # ee_client_auth_demo
 Google Earth Engine App (App Engine) using the Python API and OAuth 2.0 Client ID authentication
+
+### Usage
+
+This is tested using a Debian Linux SO (Ubuntu, Mint, etc)
+
+1. You need to have a whiteliste email account in Google Earth Engine (https://earthengine.google.com/)
+2. You need to have running the Python API (https://developers.google.com/earth-engine/python_install)
+3. You need to follow instructions in *Custom Applications* (https://developers.google.com/earth-engine/app_engine_intro). In Step 2, follow the *OAuth 2.0 Client ID* part.
+4. Clone this repo:
+
+`git clone https://github.com/fitoprincipe/ee_client_auth_demo`
+
+5. Rename the json private key to: `privatekey.json` and paste it to the root folder of the app `../ee_client_auth_demo/privatekey.json`
+6. Open a terminal in the same folder and run
+
+`dev_appserver.py app.yaml`
+
+7. Open an Internet browser and go to http://localhost:8000/
---
+++ server.py
@@ -0,0 +1,50 @@
+import webapp2
+import ee
+import ee.oauth
+from oauth2client.client import flow_from_clientsecrets
+
+SCOPE = ee.oauth.SCOPE
+flow = flow_from_clientsecrets('privatekey.json',
+                               scope=SCOPE,
+                               redirect_uri='http://localhost:8080/oauth2callback')
+flow.approval_prompt = 'force'
+
+class AuthHandler(webapp2.RequestHandler):
+    """ Handle authorization """
+    def get(self):
+        auth_url = flow.step1_get_authorize_url()
+        return webapp2.redirect(unicode.encode(auth_url))
+
+class Main(webapp2.RequestHandler):
+    """ Handle oauth2callback """
+    def get(self):
+        # get code
+        cod = self.request.get('code')
+
+        # get credentials
+        credentials = flow.step2_exchange(cod)
+
+        # initialize with credentials
+        ee.Initialize(credentials)
+
+        return webapp2.redirect("/redirected")
+
+class Redirected(webapp2.RequestHandler):
+    """ Main Handler. Here goes the main code for the page """
+    def get(self):
+
+        # Simple example code
+        i = ee.Image.constant(0).getInfo()
+        return self.response.out.write(i)
+
+app = webapp2.WSGIApplication([
+    webapp2.Route('/', AuthHandler),
+    webapp2.Route(r'/oauth2callback', Main),
+    webapp2.Route('/redirected', Redirected)
+], debug=True)
+
+def main():
+    app.run()
+
+if __name__ == '__main__':
+    main()
